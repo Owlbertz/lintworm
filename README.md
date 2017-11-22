c3455aa911a2424d89fd705a0f7b994d81e1d491
Marius Olbertz <marius.olbertz@gmail.com>
2017-11-22 22:08:11 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,68 @@
+# Lintworm
+
+Linting of all file types based on keywords.
+
+## Usage
+```javascript
+gulp.task('lintworm', function() {
+    return gulp.src(['./src/**/*'])
+           .pipe(lintworm(strings, options));
+});
+```
+
+## Parameter
+The following parameter can be passed to the plugin:
+
+### strings
+The strings to find in the given files.
+
+This can either be flat array of strings or regular expressions:
+```javascript
+['TODO', /[Ff]ixme/]
+```
+
+or it can be an array of objects:
+```javascript
+[{
+    string: 'TODO',
+    level: 'error' // Override default level for this string
+}]
+```
+
+### options
+The following options can be passed additionally:
+
+#### level
+The default level of reported findings.
+
+Default value is `'warn'`.
+
+#### failOnError
+Whether the task should fail if at least on finding with error level is found.
+
+Default value is `true`.
+
+## Full example
+The following gulp-task
+```javascript
+gulp.task('lintworm', function() {
+    return gulp.src(['./src/**/*'])
+        .pipe(lintworm([
+            // Warn for TODOs in general
+            'TODO',
+            // Warn for @ts-ignore statements that contain an explanation
+            /@ts-ignore\s+\w+/,
+            // Error on @ts-ignore statements that do not contain an explanation
+            {string: /@ts\-ignore$/, level: 'error'}
+        ]));
+});
+```
+might output this:
+```
+/path/to/files/src/index.js (2)
+	[25]	// TODO: A dynamically upper bound is required.
+	[30]	// @ts-ignore
+
+------------------------------------------------------------
+Found 1 error and 1 warning.
+```
---
+++ index.js
@@ -0,0 +1,121 @@
+var es = require('event-stream');
+var colors = require('colors');
+var PluginError = require('gulp-util').PluginError;
+var through = require('through2');
+
+var LOG_LEVELS = {
+  WARN: 'warn',
+  ERROR: 'error'
+};
+/**
+ * Gulp plugin to check files passed within the event stream for given keywords.
+ * 
+ * @param {any} searchStrings Keywords to look for.
+ * @param {any} options Optional options to modify the behavior.
+ * @returns Gulp plugin 
+ */
+var lintworm = function (searchStrings, options) {
+  options = Object.assign({
+    failOnError: true, // Whether to exit with non 0 code when at least one error is reported
+    level: LOG_LEVELS.WARN // Default log level (possible values are: 'warn', 'error')
+  }, options);
+
+  var overallFindingsPerLevel = {};
+  overallFindingsPerLevel[LOG_LEVELS.ERROR] = 0;
+  overallFindingsPerLevel[LOG_LEVELS.WARN] = 0;
+
+  /**
+   * Run the overall report.
+   * Will emit an error if conditions are met.
+   * 
+   */
+  function report() {
+    var summary = '------------------------------------------------------------\n';
+    summary += 'Found ' + overallFindingsPerLevel[LOG_LEVELS.ERROR] + ' errors and ' + overallFindingsPerLevel[LOG_LEVELS.WARN] + ' warnings.';
+    console.log(summary[overallFindingsPerLevel[LOG_LEVELS.ERROR] ? 'red' : 'yellow']);
+    if (overallFindingsPerLevel[LOG_LEVELS.ERROR] && options.failOnError) {
+      this.emit('error', new PluginError('lintworm', 'Found ' + overallFindingsPerLevel[LOG_LEVELS.ERROR] + ' errors and ' + overallFindingsPerLevel[LOG_LEVELS.WARN] + ' warnings.'));
+    }
+  };
+
+  /**
+   * Checks the given file based on the search strings provided as arguments.
+   * 
+   * @param {any} file File to check.
+   * @param {any} encoding Encoding of the file.
+   * @param {any} cb Callback to call when finished.
+   */
+  function checkFile(file, encoding, cb) {
+    if (file.isBuffer()) {
+      var fileContent = file.contents.toString();
+      var lines = fileContent.toString(encoding).split(/\r\n|[\n\r\u0085\u2028\u2029]/g);
+      var findings = [];
+      var currentLineNumber = 0;
+  
+      while (currentLineNumber < lines.length) { // Check for each line
+        var currentLine = lines[currentLineNumber];
+        for (var i = 0; i < searchStrings.length; i++) { // Check for each seach string
+          var currentSearchStringValue = null;
+          var level = options.level;
+  
+          if (typeof searchStrings[i] === 'object' && searchStrings[i].hasOwnProperty('string')) { // Object
+            currentSearchStringValue = searchStrings[i].string;
+            if (searchStrings[i].hasOwnProperty('level')) {
+              level = searchStrings[i].level;
+            }
+          } else { // String or regular expression
+            currentSearchStringValue = searchStrings[i];
+          }
+          if (!currentSearchStringValue) {
+            continue;
+          }
+  
+          var characterPositionNumber = -1;
+          if (typeof currentSearchStringValue === 'string') { // String
+            characterPositionNumber = currentLine.indexOf(currentSearchStringValue);
+          } else { // Regular expression
+            characterPositionNumber = currentLine.search(currentSearchStringValue);
+          }
+          if (characterPositionNumber !== -1) { // Current line contains current search word
+            findings.push({
+              line: currentLineNumber + 1,
+              column: characterPositionNumber,
+              fullLine: currentLine.trim(),
+              level: level,
+              string: currentSearchStringValue
+            });
+          }
+        }
+        currentLineNumber++;
+      }
+  
+  
+      if (findings.length > 0) {
+        var output = '';
+        var outputForAllFindings = '';
+        var outputTitle = file.path + ' (' + findings.length + ')\n';
+        var errorFoundForFile = false;
+        for (var f in findings) {
+          var finding = findings[f];
+          var outputForFinding = '\t' + '[' + finding.line + ']\t' + finding.fullLine +  '\n';
+          outputForAllFindings += outputForFinding[finding.level === LOG_LEVELS.ERROR ? 'red' : 'yellow'];
+          overallFindingsPerLevel[finding.level]++;
+  
+          if (finding.level === LOG_LEVELS.ERROR) {
+            errorFoundForFile = true;
+          }
+        }
+  
+        output += outputTitle[errorFoundForFile ? 'red' : 'yellow']; // Make title red if at least one error appeared in that file
+        output += outputForAllFindings;
+  
+        console.log(output);
+      }
+      cb(null, file);
+    }
+  }
+
+  return through.obj(checkFile, report);
+}
+
+module.exports = lintworm;
